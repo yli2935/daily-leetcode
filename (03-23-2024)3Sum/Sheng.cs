058bb9016ac802c9b50385aa2329f3e492769df4
sheng <chrisxin911@hotmail.com>
2024-03-23 11:24:42 -0400

    C# version

--- (03-23-2024)3Sum/Sheng.cs
+++ (03-23-2024)3Sum/Sheng.cs
@@ -1,139 +1,166 @@
-﻿
-using System.Globalization;
-
-namespace _3Sum
-{
-    internal class Program
+﻿public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+           if (nums == null || nums.Length <3)
     {
-        static void Main(string[] args)
+        return [];
+    }
+
+    List<int[]> temp = new List<int[]>();
+   
+    int zeros = 0;
+    Dictionary<int,int> lessThanZero = new Dictionary<int,int>();
+    Dictionary<int,int> largerThanZero= new Dictionary<int,int>();
+  
+   
+    List<int> lessThanZeroArray= new ();
+    List<int> largerThanZeroArray= new ();
+
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+      
+        if (nums[i] == 0)
         {
-            int[] nums = [-1, 0, 1, 2, -1, -4];
-            int[][] res = ThreeSum(nums);
+            zeros++;
+        }
+        if (nums[i] > 0)
+        {
+
+            if (!largerThanZero.ContainsKey(nums[i]))
+            {
+                largerThanZero[nums[i]] = 1;
+                largerThanZeroArray.Add(nums[i]);
+            }
+            else if (largerThanZero[nums[i]] == 1)
+            {
+                largerThanZero[nums[i]]++;
+                largerThanZeroArray.Add(nums[i]);
+            }
+            //We do not need to care about more than 2 situation.
+            
+        }
+        if (nums[i] < 0)
+        {
+            if (!lessThanZero.ContainsKey(nums[i]))
+            {
+                lessThanZero[nums[i]] = 1;
+                lessThanZeroArray.Add(nums[i]);
+            }
+            else if (lessThanZero[nums[i]] == 1)
+            {
+                lessThanZero[nums[i]]++;
+                lessThanZeroArray.Add(nums[i]);
+            }
+        }
+        
+    }
+
+    if (zeros>=3 ) {
+        var temporaryTriplet = new int[] { 0, 0, 0 };
+        temp.Add(temporaryTriplet);
+    }
+    //if there are 2 zeros, we consider it as 1 zero, the others shoud be one is less than 0, one is bigger than 0
+    if(zeros>0 )
+    {
+        foreach (var key in lessThanZero.Keys)
+        {
+            if (largerThanZero.ContainsKey(0 - key))
+                {
+                var temporary = new int[] { key,0,0-key };
+                temp.Add(temporary);
+            }
+        }
+    }
+    //if no zero in the nums
+  
+        //two from right,one from left
+        if (largerThanZeroArray.Count >= 2)
+        {
+        //get two of the right
+        List<int> largeKeys = []; 
+        //if there are duplicate number is it
+        foreach (var key in largerThanZero.Keys)
+        {
+            if (largerThanZero[key] == 2)
+            {
+                if (lessThanZero.ContainsKey(0 - key - key))
+                {
+                    var temporary = new int[] { 0 - key - key, key, key };
+                    temp.Add(temporary);
+                }
+            }
+            
+            largeKeys.Add(key);
+            
         }
 
-        //Sort first and classify situation base on the 0
-        private static int[][] ThreeSum(int[] nums)
-        {
-            //validate nums 
-            if (nums == null || nums.Length <3)
+        // no duplicat key
+
+
+            for(int i = 0;i < largeKeys.Count - 1;i++)
             {
-                return [];
-            }
-
-            List<int[]> temp = new List<int[]>();
-           
-            int zeros = 0;
-            Dictionary<int,int> lessThanZero = new Dictionary<int,int>();
-            Dictionary<int,int> largerThanZero= new Dictionary<int,int>();
-          
-           
-            List<int> lessThanZeroArray= new ();
-            List<int> largerThanZeroArray= new ();
-
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-              
-                if (nums[i] == 0)
+                for (int j = i+1;j< largeKeys.Count; j++)
                 {
-                    zeros++;
-                }
-                if (nums[i] > 0)
-                {
-
-                    if (!largerThanZero.ContainsKey(nums[i]))
+                    if (lessThanZero.ContainsKey(0 - largeKeys[j] - largeKeys[i]))
                     {
-                        largerThanZero[nums[i]] = 1;
-                        largerThanZeroArray.Add(nums[i]);
-                    }
-                    else if (largerThanZero[nums[i]] == 1)
-                    {
-                        largerThanZero[nums[i]]++;
-                        largerThanZeroArray.Add(nums[i]);
-                    }
-                    //We do not need to care about more than 2 situation.
-                    
-                }
-                if (nums[i] < 0)
-                {
-                    if (!lessThanZero.ContainsKey(nums[i]))
-                    {
-                        lessThanZero[nums[i]] = 1;
-                        lessThanZeroArray.Add(nums[i]);
-                    }
-                    else if (lessThanZero[nums[i]] == 1)
-                    {
-                        lessThanZero[nums[i]]++;
-                        lessThanZeroArray.Add(nums[i]);
-                    }
-                }
-                
-            }
-
-            if (zeros>=3 ) {
-                var temporaryTriplet = new int[] { 0, 0, 0 };
-                temp.Add(temporaryTriplet);
-            }
-            //if there are 2 zeros, we consider it as 1 zero, the others shoud be one is less than 0, one is bigger than 0
-            if(zeros==1 || zeros==2 )
-            {
-                foreach (var key in lessThanZero.Keys)
-                {
-                    if (largerThanZero.ContainsKey(0 - key))
+                        var temporary = new int[]
                         {
-                        var temporary = new int[] { 0, key,0-key };
+                            largeKeys [j],largeKeys[i],0 - largeKeys[j] - largeKeys[i]
+                        };
                         temp.Add(temporary);
                     }
+                    
                 }
             }
-            //if no zero in the nums
-          
-                //two from right,one from left
-                if (largerThanZeroArray.Count >= 2)
-                {
-                    //get two of the right
-                    for(int i = 0;i < largerThanZeroArray.Count - 1;i++)
-                    {
-                        for (int j = i+1;j<largerThanZeroArray.Count; j++)
-                        {
-                            if (lessThanZero.ContainsKey(0 - largerThanZeroArray[j] - largerThanZeroArray[i]))
-                            {
-                                var temporary = new int[]
-                                {
-                                    largerThanZeroArray [j],largerThanZeroArray[i],0 - largerThanZeroArray[j] - largerThanZeroArray[i]
-                                };
-                                temp.Add(temporary);
-                            }
-                            
-                        }
-                    }
-                }
-
-
-                //find two from left, one from right
-               
-                if (lessThanZeroArray.Count >= 2)
-                {
-                    //get two of the right
-                    for (int i = 0; i < lessThanZeroArray.Count - 1; i++)
-                    {
-                        for (int j = i + 1; j < lessThanZeroArray.Count; j++)
-                        {
-                            if (largerThanZero.ContainsKey(0 - lessThanZeroArray[j] - lessThanZeroArray[i]))
-                            {
-                                var temporary = new int[]
-                                {
-                                    lessThanZeroArray [j],lessThanZeroArray[i],0 - lessThanZeroArray[j] - lessThanZeroArray[i]
-                                };
-                                temp.Add(temporary);
-                            }
-
-                        }
-                    }
-                }
-
-
-            return temp.ToArray();
         }
+
+
+        //find two from left, one from right
+       
+        if (lessThanZeroArray.Count >= 2)
+        {
+        //get two of the right
+        //get two of the right
+        List<int> lessKeys = [];
+        //if there are duplicate number is it
+        foreach (var key in lessThanZero.Keys)
+        {
+            if (lessThanZero[key] == 2)
+            {
+                if (largerThanZero.ContainsKey(0 - key - key))
+                {
+                    var temporary = new int[] { key, key , 0 - key - key};
+                    temp.Add(temporary);
+                }
+            }
+
+            lessKeys.Add(key);
+
+        }
+
+        // no duplicate key
+
+
+        for (int i = 0; i < lessKeys.Count - 1; i++)
+        {
+            for (int j = i + 1; j < lessKeys.Count; j++)
+            {
+                if (largerThanZero.ContainsKey(0 - lessKeys[j] - lessKeys[i]))
+                {
+                    var temporary = new int[]
+                    {
+                            lessKeys [j],lessKeys[i],0 - lessKeys[j] - lessKeys[i]
+                    };
+                    temp.Add(temporary);
+                }
+
+            }
+        }
+    
+}
+
+
+    return temp.ToArray();
+        
     }
 }
