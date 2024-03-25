361f2f3aff9f16852d0a836f4ac2fba6a312902e
xixvtt <louiselao64@gmail.com>
2024-03-25 16:59:40 -0400

    pyversion lc56

---
+++ (03-25-2024)Merge Intervals/louis.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        intervals.sort(key= lambda x:x[0])
+
+        for interval in intervals:
+            # res empty or no need to merge 
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res 
