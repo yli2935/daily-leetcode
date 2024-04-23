5ff40e852874f238d91fdbcd336a3246388c8c20
sheng <chrisxin911@hotmail.com>
2024-04-23 13:51:07 -0400

    c# version from Sheng

---
+++ (04-23-2024)Rotting Oranges/sheng.cs
@@ -0,0 +1,77 @@
+﻿
+namespace RottingOranges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = [[2, 1, 1], [1, 1, 1], [0, 1, 2]];
+            int res = OrangesRotting(grid);
+            Console.WriteLine(res);
+        }
+
+        private static int OrangesRotting(int[][] grid)
+        {
+            int minute = 0;
+            Queue<((int, int), int)> rottenOranges = [];
+            bool[][] visited = new bool[grid.Length][];
+            int[][] directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+            int m = grid.Length;
+            int n = grid[0].Length;
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                visited[i] = new bool[grid[i].Length];
+                for (int j = 0; j < grid[i].Length; j++)
+                    visited[i][j] = false;
+            }
+            //put current rotten oranges in the queque
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (visited[i][j] == false && grid[i][j] == 2)
+                    {
+                        rottenOranges.Enqueue(((i, j), minute));
+                        visited[i][j] = true;
+                    }
+                }
+            }
+            while (rottenOranges.Count > 0)
+            {
+                var orange = rottenOranges.Dequeue();
+                int x = orange.Item1.Item2;
+                int y = orange.Item1.Item1;
+                int temporaryMinute = orange.Item2;
+                foreach (var direction in directions)
+                {
+                    int newX = x + direction[1];
+                    int newY = y + direction[0];
+                    if (newX >= 0 && newY >= 0 && newX < n && newY < m && visited[newY][newX] == false && grid[newY][newX] == 1)
+                    {
+                        grid[newY][newX] = 2;
+                        rottenOranges.Enqueue(((newY, newX), temporaryMinute+1));
+                        visited[newY][newX] = true;
+                        minute = temporaryMinute+1;
+
+                    }
+
+
+                }
+
+            }
+            //check if there a fresh orange left
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return minute;
+        }
+    }
+}
